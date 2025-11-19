8dc08759b393ce680c2d32eb66a2ee3fdd033f13
business-partnership <abusinesspartnership@gmail.com>
2025-11-19 22:17:27 +0600

    dashboard file modifyed

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div className="flex justify-center items-center h-screen text-primary text-2xl bg-[#ecebe5]">
+    <div className='flex justify-center items-center h-screen text-primary text-2xl bg-[#ecebe5] flex-col'>
       <h1>Welcome from our dashboard</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat optio
+        voluptatum delectus ipsum officiis quis? Quia aliquam, incidunt
+        veritatis saepe placeat rerum aliquid tempora earum consectetur,
+        corporis ipsum aut praesentium!
+      </p>
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
